b957e44cf163b2bcf32d96199884acaff237cf91
Saed Swedan <saed.swedan@gmail.com>
2025-08-31 11:29:14 +0300

    make the reset button reset the language

--- src/views/menu/reset.ts
+++ src/views/menu/reset.ts
@@ -1,11 +1,17 @@
 import { saveUserSettings, userSettings } from "@/globals/userSettings";
 import runAccessibility from "./runAccessibility";
+import { getDefaultLanguage } from "@/i18n/getDefaultLanguage";
+import { changeLanguage } from "@/i18n/changeLanguage";
+
 
 export default function reset() {
     document?.querySelectorAll(".asw-selected")?.forEach(el => el?.classList?.remove("asw-selected"))
 
     userSettings.states = {};
+    //userSettings.lang = ;
+    
     runAccessibility();
-
     saveUserSettings();
+
+    changeLanguage(getDefaultLanguage());
 }
